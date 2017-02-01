772b878760827125646c5b60e28b9126c5be01f4
Aunitz Giménez <aunitz@adimedia.net>
2017-02-01 16:23:04 +0100

    Push Messaging and Notification Sample
    https://github.com/GoogleChrome/samples/tree/gh-pages/push-messaging-and-notifications

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAA8UEcivI:APA91bEgEEVuittINZHr2TEhEUvkjOqP6FL7yaGqbbh7zUZ-s8_tK1MPiYbjfYTw_VQ3hYRUKi1sKWO4uxm0L8NUMqXmaYI4MgC2Ss-X0F_pbrhE4ktrWzpI1Nre4ufUbkIUdBEfLL9Z'
+};
